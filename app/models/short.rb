18a7d7f62949564a9bc50c019d663bede6774f70
jfarah <jfarah@libano>
2010-10-17 08:40:17 -0200

    remocao de require.

--- app/models/short.rb
+++ app/models/short.rb
@@ -1,11 +1,8 @@
-require 'thread'
-
 class Short < ActiveRecord::Base
   has_many :resolveds
 
    def self.getLimitedUnprocessedList(lastIndex, limit)
        self.find_by_sql("select * from trendlinks.shorts where id > #{lastIndex} and resolved_id is null limit #{limit}")
    end
-
 
 end
