d93d7066f98750df28ed3f89f876c99ef49f3d17
jfarah <jfarah@libano>
2010-10-17 06:54:29 -0200

    alteracao na logica com threadPool

--- script/resolverProcessor.rb
+++ script/resolverProcessor.rb
@@ -1,65 +1,54 @@
-#!/usr/bin/env ruby
-
 require 'rubygems'
 require 'net/http'
 require 'uri'
 
-ENV["RAILS_ENV"] ||= "development" 
-ENV["PATH"] = "/usr/local/bin/:/opt/local/bin:#{ENV["PATH"]}"
-require File.dirname(__FILE__)+'/../config/environment'
-Rails.configuration.log_level = :info # Disable debug
-ActiveRecord::Base.allow_concurrency = true
+def processShortList(list)
+  begin
+    first_id = list[0].id
+    last_id = list[-1].id
+      
+    list.each { |s|
+      puts "Thread #{first_id} #{last_id} resolving short id: #{s.id} url: #{s.url}"
+      s_url = s.url;
 
-DEFAULT_SHORTS_LIMIT = 1000;
-
-#TODO - log
-
-def processShortList()
-  
-  #logger.debug "processShortList..."
-
-  list = getShortList(nil);
-  list.each { |s|
-    s_url = s.url;
-
-    resolved = getResolvedURL(s_url);
-    s.resolved_id = resolved.id
-    s.save
-
-    #logger.debug("ShortList processed.")
-  }
+      resolved = getResolvedURL(s_url);
+      if(resolved != nil)
+        s.resolved_id = resolved.id
+        s.save
+      end
+    }
+  rescue  Exception => e
+    puts "Thread #{first_id} #{last_id} #{e.message}"
+  end  
 end
 
-def getShortList(limit)
-  limit = DEFAULT_SHORTS_LIMIT if(limit == nil or limit == 0)
-  #logger.debug("getting short list by limit "+limit.to_s)
-  return Short.getLimitedUnprocessedList(limit)
-end
 
+#Resolve uma url encurtada e retorna um registro Resolved referente a essa url,
+#um novo registro Resolved eh criado caso nao enenhum exista para aquela url.
 def getResolvedURL(s_url)
   r_url = fetch(s_url);
-
-  resolved = Resolved.find_by_resolved_url r_url;
-  if(resolved == nil)
-    resolved = Resolved.new
-    resolved.resolved_url = r_url;
-    resolved.save
+  if(r_url != "-1")
+    resolved = Resolved.find_by_resolved_url r_url;
+    if(resolved == nil)
+      resolved = Resolved.new
+      resolved.resolved_url = r_url;
+      resolved.save
+    end
+    resolved
   end
-
-  resolved
+  nil
 end
 
+#dada uma url encurtada, retorna a url original
 def fetch(uri_str, limit = 10)
-      begin       
-        response = Net::HTTP.get_response(URI.parse(uri_str))
-        case response
-          when Net::HTTPMovedPermanently then response['location']
-          else
-            "-1"
-        end
-      rescue Exception
-        "-1"
-      end
+  begin
+    response = Net::HTTP.get_response(URI.parse(uri_str))
+    case response
+    when Net::HTTPMovedPermanently then response['location']
+    else
+      "-1"
+    end
+  rescue Exception
+    "-1"
+  end
 end
-
-processShortList()
