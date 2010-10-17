f09f097ce08508360d04070d74c784a4d3d8a75a
jfarah <jfarah@libano>
2010-10-17 06:53:42 -0200

    alteracao na query para busca a partir de um id.

--- app/models/short.rb
+++ app/models/short.rb
@@ -1,8 +1,11 @@
+require 'thread'
+
 class Short < ActiveRecord::Base
   has_many :resolveds
-  
-  def self.getLimitedUnprocessedList(limit)
-    self.find_by_sql("select * from trendlinks.shorts limit #{limit}")
-  end
+
+   def self.getLimitedUnprocessedList(lastIndex, limit)
+       self.find_by_sql("select * from trendlinks.shorts where id > #{lastIndex} and resolved_id is null limit #{limit}")
+   end
+
 
 end
