360ffa0dc918394fcc96d8421d5d4a5a158cae9e
Diego <dnakamashi@bananal.(none)>
2010-10-17 05:05:31 -0200

    tops

--- app/controllers/trend_controller.rb
+++ app/controllers/trend_controller.rb
@@ -1,17 +1,7 @@
 class TrendController < ApplicationController
 
 def index
-	@last_hour_tops = Array.new
-	@last_hour_tops.push('http://www.globo.com')
-	@last_hour_tops.push('http://www.globo.com1')
-	@last_hour_tops.push('http://www.globo.com2')
-	@last_hour_tops.push('http://www.globo.com3')
-	@last_hour_tops.push('http://www.globo.com3')
-	@last_hour_tops.push('http://www.globo.com4')
-	@last_hour_tops.push('http://www.globo.com5')
-	@last_hour_tops.push('http://www.globo.com6')
-	@last_hour_tops.push('http://www.globo.com7')
-	@last_hour_tops.push('http://www.globo.com9')
+	@last_hour_tops = Top.find(:all, :order => 'count', :limit => 10)
 end
 
 end
